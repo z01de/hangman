49d9a5c20d1b5de4bd915737170796026d155864
z01de <46068015+z01de@users.noreply.github.com>
2024-11-15 00:09:51 +0300

    Update Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,6 @@
 public class Launcher {
     public static void main(String[] args) {
-        WordBook wordBook = new WordBook("C:\\Games\\words.txt");
+        WordBook wordBook = new WordBook("C:\\Games\\HangmanGame\\Words.txt");
         Game hangman = new Game(wordBook);
         hangman.launch();
     }
